e7147907e41c5d98c5bcc5c01ef9dc659c93b306
GuillaumeDonzeau <gdonzeau@hotmail.com>
2021-07-05 16:13:56 +0200

    Initializing with recipe's API

--- Reciplease/Reciplease/Controller/ViewController.swift
+++ Reciplease/Reciplease/Controller/ViewController.swift
@@ -9,19 +9,22 @@ import UIKit
 import Alamofire
 
 class ViewController: UIViewController {
-    let adressUrl = "https://httpbin.org/headers"
+    let adressUrl = "https://api.edamam.com/api/recipes/v2?type=public"
+    
     let headers: HTTPHeaders = [
-        .authorization(username: "test@email.com", password: "testpassword"),
+        .authorization(username: Keys.id.rawValue, password: Keys.key.rawValue),
         .accept("application/json")
     ]
-    let parameters = ["category": "Movies", "genre": "Action"]
+    let parameters = ["q": "Chicken,Tomatoes", "diet": "balanced"]
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        AF.request(adressUrl, parameters: parameters, headers: headers).responseJSON {response in
-            debugPrint(response)    }    }
-
-
+        let finalAdressUrl = adressUrl
+        AF.request(finalAdressUrl, parameters: parameters, headers: headers).responseJSON {response in
+            debugPrint(response)
+            
+        }
+    }
 }
 
---
+++ Reciplease/Reciplease/OtherFiles/APIKeys.swift
@@ -0,0 +1,14 @@
+//
+//  APIKeys.swift
+//  Reciplease
+//
+//  Created by Guillaume Donzeau on 05/07/2021.
+//
+
+import Foundation
+
+enum Keys: String {
+    
+    case id = "Change with your id"
+    case key = "Change with your key"
+}
